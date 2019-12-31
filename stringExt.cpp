f300c062d64c75115628af8b510dc091f3f6d2c8
Sinusoid314 <sinusoid314@gmail.com>
2019-12-31 18:16:15 -0500

    Add files via upload

---
+++ stringExt.cpp
@@ -0,0 +1,152 @@
+#include <sstream>
+#include "stringExt.h"
+
+using namespace std;
+
+
+string MidStr(const string& inStr, unsigned int cutPos, unsigned int cutLen)
+//Return a substring from the given string
+{
+    if(cutPos >= inStr.length())
+    {
+        return string("");
+    }
+    else
+    {
+        return inStr.substr(cutPos, cutLen);
+    }
+}
+
+string LeftStr(const string& inStr, unsigned int cutLen)
+//Return a substring from the beginning of the given string
+{
+	return inStr.substr(0, cutLen);
+}
+
+string RightStr(const string& inStr, unsigned int cutLen)
+//Return a substring from the end of the given string
+{
+    if(cutLen == 0)
+    {
+        return string("");
+    }
+    else if(cutLen < inStr.length())
+    {
+        return inStr.substr(inStr.length()-cutLen);
+    }
+    else
+    {
+	    return inStr;
+	}
+}
+
+string TrimStr(const string& inStr)
+//Return the given string with space characters cut from both ends
+{
+    unsigned int cutPos;
+    unsigned int cutLen;
+
+    cutPos = inStr.find_first_not_of(' ');
+
+    if(cutPos != string::npos)
+    {
+        cutLen = inStr.find_last_not_of(' ') - cutPos + 1;
+        return inStr.substr(cutPos, cutLen);
+    }
+    else
+    {
+        return string("");
+    }
+
+    return inStr;
+}
+
+string LTrimStr(const string& inStr)
+//Return the given string with space characters cut from the beginning
+{
+    unsigned int cutPos;
+
+    cutPos = inStr.find_first_not_of(' ');
+
+    if(cutPos != string::npos)
+    {
+        return inStr.substr(cutPos);
+    }
+    else
+    {
+        return string("");
+    }
+}
+
+string RTrimStr(const string& inStr)
+//Return given string with space characters cut from the end
+{
+    unsigned int cutPos;
+
+    cutPos = inStr.find_last_not_of(' ');
+
+    if(cutPos != string::npos)
+    {
+        return inStr.substr(0, cutPos + 1);
+    }
+    else
+    {
+        return string("");
+    }
+}
+
+string UCaseStr(const string& inStr)
+//Return given string with all upper-case letters
+{
+    string outStr = inStr;
+
+    for(unsigned int n=0; n<inStr.length(); n++)
+    {
+        if( (int(inStr[n]) >= 97) && (int(inStr[n]) <= 122) )
+        {
+            outStr[n] = char( int(inStr[n]) - 32 );
+        }
+    }
+
+    return outStr;
+}
+
+string LCaseStr(const string& inStr)
+//Return given string with all lower-case letters
+{
+	string outStr = inStr;
+
+    for(unsigned int n=0; n < inStr.length(); n++)
+    {
+        if( (int(inStr[n]) >= 65) && (int(inStr[n]) <= 90) )
+        {
+            outStr[n] = char( int(inStr[n]) + 32 );
+        }
+    }
+
+    return outStr;
+}
+
+string NumToStr(double num)
+//Convert the given number into a string object
+{
+    return static_cast<ostringstream*>(&(ostringstream() << num))->str();
+}
+
+double StrToNum(const string& str)
+//Convert a string object to a number
+{
+	double tmpNum;
+    istringstream(str) >> tmpNum;
+    return tmpNum;
+}
+
+bool IsNumericStr(const string& str)
+//Find out if a string contains a number
+{
+    stringstream tmpStream;
+    double tmpNum;
+    tmpStream << str;
+    tmpStream >> tmpNum;
+    return tmpStream.eof();
+}
---
+++ stringExt.h
@@ -0,0 +1,22 @@
+//String Extension v1.01
+//Written by Andrew Sturges
+//April 2015
+
+#ifndef _BASIC_STRINGEXT_H
+#define _BASIC_STRINGEXT_H
+
+#include <string>
+
+std::string MidStr(const std::string&, unsigned int, unsigned int cutLen = std::string::npos);
+std::string LeftStr(const std::string&, unsigned int);
+std::string RightStr(const std::string&, unsigned int);
+std::string TrimStr(const std::string&);
+std::string LTrimStr(const std::string&);
+std::string RTrimStr(const std::string&);
+std::string UCaseStr(const std::string&);
+std::string LCaseStr(const std::string&);
+std::string NumToStr(double);
+double StrToNum(const std::string&);
+bool IsNumericStr(const std::string&);
+
+#endif
